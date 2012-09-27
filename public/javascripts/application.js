e47912e9bed2ef6c9dc0bb4123d2ef06abdf70c4
Shicholas <nicholas.shook@gmail.com>
2012-09-27 00:03:40 -0700

    added print button/js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,3 +13,25 @@ $('#sendall').click(function() {
         $.post(this.action, $form.serialize());
     });
 });
+
+// When the document is ready, initialize the link so
+// that when it is clicked, the printable area of the
+// page will print.
+$(document).ready(function(){
+ 
+// Hook up the print link.
+$( ".print" )
+.attr( "href", "javascript:void( 0 )" )
+.click(
+function(){
+// Print the DIV.
+$( ".printable" ).print();
+ 
+// Cancel click event.
+return( false );
+}
+)
+;
+ 
+}
+);
